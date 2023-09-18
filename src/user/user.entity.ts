9fce9ca89b0152c2f8abe81e443f82501a9beaae
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-09-18 16:59:27 -0300

    feat(user): add user entity

--- src/user/user.entity.ts
+++ src/user/user.entity.ts
@@ -1,17 +1,27 @@
-import { Field, ObjectType } from '@nestjs/graphql';
-import { User } from '@prisma/client';
+import { Field, Int, ObjectType } from '@nestjs/graphql';
+import { Column, Entity, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { Post } from 'src/posts/entities/post.entity';
 
+@Entity()
 @ObjectType()
-export class UserEntity implements User {
-  @Field()
-  id: string;
+export class UserEntity {
+  @PrimaryGeneratedColumn()
+  @Field(() => Int)
+  id: number;
 
+  @Column()
   @Field()
   name: string;
 
+  @Column()
   @Field()
   email: string;
 
+  @Column()
   @Field()
   password: string;
+
+  @OneToMany(() => Post, (post) => post.user)
+  @Field(() => [Post], { nullable: true })
+  posts?: Post[];
 }
