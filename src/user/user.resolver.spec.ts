64dfcdc4964fd347a8540d4cd399221504d800d8
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-09-21 11:59:52 -0300

    test(user): add user service and resolver tests

--- src/user/user.resolver.spec.ts
+++ src/user/user.resolver.spec.ts
@@ -1,18 +1,87 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserResolver } from './user.resolver';
+import { UserService } from './user.service'; // Import UserService if not already imported
+import { UserEntity } from './entities/user.entity';
 
 describe('UserResolver', () => {
   let resolver: UserResolver;
+  let userService: UserService; // Add userService for mocking
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserResolver],
+      providers: [
+        UserResolver,
+        {
+          provide: UserService, // Provide a mock instance of UserService
+          useValue: {
+            findOne: jest.fn(),
+            findAll: jest.fn(),
+            createUser: jest.fn(),
+          } as Partial<UserService>,
+        },
+      ],
     }).compile();
 
     resolver = module.get<UserResolver>(UserResolver);
+    userService = module.get<UserService>(UserService);
   });
 
   it('should be defined', () => {
     expect(resolver).toBeDefined();
+  });
+
+  describe('getUser', () => {
+    it('should return a user entity', async () => {
+      const userId = 1;
+      const mockUser: UserEntity = {
+        id: userId,
+        name: 'Test User',
+        email: 'test@gmail.com',
+        password: 'password',
+      };
+
+      (userService.findOne as jest.Mock).mockResolvedValue(mockUser);
+
+      const result = await resolver.getUser(userId);
+
+      expect(userService.findOne).toHaveBeenCalledWith(userId);
+      expect(result).toBe(mockUser);
+    });
+  });
+
+  describe('users', () => {
+    it('should receive a list of users', async () => {
+      const users = [UserEntity];
+      (userService.findAll as jest.Mock).mockResolvedValue(users);
+
+      const result = await resolver.users();
+
+      expect(userService.findAll).toHaveBeenCalled();
+      expect(result).toBe(users);
+    });
+  });
+
+  describe('createUser', () => {
+    it('should create a new user', async () => {
+      const createUserInput = {
+        name: 'Test User',
+        email: 'test@gmail.com',
+        password: 'password',
+      };
+
+      const createdUser: UserEntity = {
+        id: 1,
+        name: 'Test User',
+        email: 'test@gmail.com',
+        password: 'password',
+      };
+
+      (userService.createUser as jest.Mock).mockResolvedValue(createdUser);
+
+      const result = await resolver.createUser(createUserInput);
+
+      expect(userService.createUser).toHaveBeenCalledWith(createUserInput);
+      expect(result).toBe(createdUser);
+    });
   });
 });
--- src/user/user.service.spec.ts
+++ src/user/user.service.spec.ts
@@ -1,18 +1,105 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UserService } from './user.service';
+import { UserEntity } from './entities/user.entity';
+import { Repository } from 'typeorm';
+import { getRepositoryToken } from '@nestjs/typeorm';
 
 describe('UserService', () => {
   let service: UserService;
+  let userRepository: Repository<UserEntity>;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [UserService],
+      providers: [
+        UserService,
+        {
+          provide: getRepositoryToken(UserEntity),
+          useClass: Repository,
+        },
+      ],
     }).compile();
 
     service = module.get<UserService>(UserService);
+    userRepository = module.get<Repository<UserEntity>>(
+      getRepositoryToken(UserEntity),
+    );
   });
 
   it('should be defined', () => {
     expect(service).toBeDefined();
+  });
+
+  describe('createUser', () => {
+    it('should create a new user', async () => {
+      const mockUserInput = {
+        name: 'Test User',
+        email: 'test@gmail.com',
+        password: 'password',
+      };
+
+      const mockUser: UserEntity = {
+        id: 1,
+        name: 'Test User',
+        email: 'test@gmail.com',
+        password: 'password',
+      };
+
+      const createSpy = jest.spyOn(userRepository, 'create');
+      const saveSpy = jest.spyOn(userRepository, 'save');
+
+      createSpy.mockReturnValue(mockUser);
+      saveSpy.mockResolvedValue(mockUser);
+
+      const result = await service.createUser(mockUserInput);
+
+      expect(createSpy).toHaveBeenCalledWith(mockUserInput);
+      expect(saveSpy).toHaveBeenCalledWith(mockUser);
+      expect(result).toEqual(mockUser);
+    });
+  });
+
+  describe('findAll', () => {
+    it('should find all users', async () => {
+      const users: UserEntity[] = [];
+
+      const findSpy = jest.spyOn(userRepository, 'find');
+      findSpy.mockResolvedValue(users);
+
+      const result = await service.findAll();
+
+      expect(findSpy).toHaveBeenCalled();
+      expect(result).toEqual(users);
+    });
+  });
+
+  describe('findOne', () => {
+    it('should find a user by ID', async () => {
+      const userId = 1;
+      const user: UserEntity = {
+        id: userId,
+        name: 'Test User',
+        email: 'test@gmail.com',
+        password: 'password',
+      };
+
+      const findOneOrFailSpy = jest.spyOn(userRepository, 'findOneOrFail');
+      findOneOrFailSpy.mockResolvedValue(user);
+
+      const result = await service.findOne(userId);
+
+      expect(findOneOrFailSpy).toHaveBeenCalledWith({ where: { id: userId } });
+      expect(result).toEqual(user);
+    });
+
+    it('should throw an error when user is not found', async () => {
+      const userId = 999;
+
+      const findOneOrFailSpy = jest.spyOn(userRepository, 'findOneOrFail');
+      findOneOrFailSpy.mockRejectedValue(new Error('User not found'));
+
+      await expect(service.findOne(userId)).rejects.toThrowError(
+        'User not found',
+      );
+    });
   });
 });
