d333c9ca4c37669fd484ef6a23896c4583ab7fbc
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-09-22 21:24:17 -0300

    feat(auth): add auth modules

---
+++ src/auth/auth.resolver.ts
@@ -0,0 +1,14 @@
+import { Args, Context, Mutation, Resolver } from '@nestjs/graphql';
+import { AuthService } from './auth.service';
+import { LoginResponse } from './dto/login-response';
+import { LoginUserInput } from './dto/login-user.input';
+
+@Resolver()
+export class AuthResolver {
+  constructor(private authService: AuthService) {}
+
+  @Mutation(() => LoginResponse)
+  login(@Args('loginUserInput') loginUserInput: LoginUserInput) {
+    return this.authService.login(loginUserInput);
+  }
+}
---
+++ src/auth/jwt.strategy.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@nestjs/common';
+import { PassportStrategy } from '@nestjs/passport';
+import { ExtractJwt } from 'passport-jwt';
+import { Strategy } from 'passport-local';
+
+@Injectable()
+export class JwtStrategy extends PassportStrategy(Strategy) {
+  constructor() {
+    super({
+      jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
+      ignoreExpiration: false,
+      secretOrKey: 'hide-me',
+    });
+  }
+
+  async validate(payload: any) {
+    return { userId: payload.userId, email: payload.email };
+  }
+}
