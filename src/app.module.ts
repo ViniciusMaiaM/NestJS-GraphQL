a786d743db58198b0394d48c93d2f822c7099270
Vinicius <viniciusmaiamarinho1@gmail.com>
2023-09-22 21:40:51 -0300

    refactor(module): calling auth module

--- src/app.module.ts
+++ src/app.module.ts
@@ -6,6 +6,7 @@ import { UserModule } from './user/user.module';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { PostsModule } from './posts/posts.module';
 import { GraphQLError, GraphQLFormattedError } from 'graphql';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -36,6 +37,7 @@ import { GraphQLError, GraphQLFormattedError } from 'graphql';
     }),
     UserModule,
     PostsModule,
+    AuthModule,
   ],
   controllers: [],
   providers: [],
